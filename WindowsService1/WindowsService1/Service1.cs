4aa10663ee10bb1d6f84e54bdeb11e7ac31cc148
Bluedin <pacome.chulejeune@viacesi.fr>
2017-03-10 11:30:20 +0100

    Création d'un service Windows
    Le service Windows écrit dans le gestionnaire d'évènement Coucou toutes
    les minutes

---
+++ WindowsService1/WindowsService1/Service1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        private static void LogWriteEvent(object source, ElapsedEventArgs e)
+        {
+            String Source = "TestService";
+            String Log = "Application";
+            String Event = "Coucou";
+
+            if (!EventLog.SourceExists(Source))
+            {
+                EventLog.CreateEventSource(Source, Log);
+            }
+
+            EventLog.WriteEntry(Source, Event);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer temp = new Timer();
+            temp.Elapsed += new ElapsedEventHandler(LogWriteEvent);
+            temp.Interval = 60000;
+            temp.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
